8b3209a738cfc0b7eb0c6825790be70fbc6cce02
ayakade <ayakade13@gmail.com>
2021-01-28 20:49:37 -0800

    add movies card

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+// set default image
 const default_image =
-  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+  "https://upload.wikimedia.org/wikipedia/en/0/00/The_Child_aka_Baby_Yoda_%28Star_Wars%29.jpg";
 
 
 const Movie = ({ movie }) => {
-  const poster =
-    movie.Poster === "N/A" ? default_image : movie.Poster;
+    const poster =
+        movie.Poster === "N/A" ? default_image : movie.Poster; // if there's no image use default image
     return (
-        <div className="movie">
-            <h2>{movie.Title}</h2>
-            <div>
-                <img
-                width="200"
-                alt={`The movie titled: ${movie.Title}`}
-                src={poster}
-                />
-            </div>
-            <p>({movie.Year})</p>
+        <div className="movie" key={movie.imdbID}>
+            <Link to={"/movie/"+movie.imdbID} >
+                <div className="image"> 
+                    <img
+                        width="200"
+                        src={poster}
+                        alt={movie.Title}
+                    />
+                </div>
+                <h2>{movie.Title}</h2>
+            </Link>
         </div>
     );
 };
